7368c18e99e575ee7d111f50850f8b0f005597b0
小瓜呆黏誠 <ncchen99@gmail.com>
2020-10-05 13:03:38 +0800

    Add files via upload

---
+++ UVA/CPE1Star/10252 - Common Permutation.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    string f,s; //first, second
+    while(cin>>f>>s) {
+        int fl[256] = {0}, sl[256] = {0};
+        for(int i=0; i<f.size(); i++)
+            fl[f[i]]++;
+        for(int i=0; i<s.size(); i++)
+            sl[s[i]]++;
+        for(int i='a'; i<='z'; i++)
+            if(fl[i]>0 && sl[i]>0)
+                for(int j=0; j<min(fl[i],sl[i]); j++)
+                    cout<<(char)i;
+        cout<<'\n';
+    }
+}
