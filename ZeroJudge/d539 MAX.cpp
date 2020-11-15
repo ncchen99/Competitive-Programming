a94cb69cb9d3305e7f55051d45a2aaf388815b96
小瓜呆黏誠 <ncchen99@gmail.com>
2020-11-15 17:39:07 +0800

    Add files via upload

---
+++ ZeroJudge/d539 MAX.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int maxn = 500001;
+int main() {
+    // ios::sync_with_stdio(0);
+    // cin.tie(0);
+    int n, t[maxn], m, s, e;
+    cin >> n;
+    int k = sqrt(n), c[n / k + 1] = {};
+    for (int i = 0; i < n; i++) cin >> t[i];
+    for (int i = 0; i < n / k * k; i += k) {
+        int ext = INT_MIN;
+        for (int j = i; j < n && j < i + k; j++) ext = max(ext, t[j]);
+        c[i / k] = ext;
+    }
+    for (int i = 0, j = 0; i < n; i += k, j++)
+        cout << j << ' ';
+    cout << endl;
+    for (int i = 0, j = 0; i < n; i += k, j++)
+        cout << c[j] << ' ';
+    cin >> m;
+    while (m--) {
+        cin >> s >> e;
+        if (e < s) swap(s, e);
+        s -= 1;
+        e -= 1;
+        int ext = INT_MIN;
+        for (int i = s; s % k && i < s + k - (s % k); i++)
+            ext = max(ext, t[i]);
+        for (int i = (s % k ? s + k - (s % k) : s);
+             i < e - (e % k); i += k) {
+            ext = max(ext, c[i / k]);
+            cout << i / k << ' ';
+        }
+        cout << endl;
+        for (int i = e - (e % k); i <= e; i++)
+            ext = max(ext, t[i]);
+        cout << ext << '\n';
+    }
+    system("pause");
+}
+/*
+20
+1 4 7 8 5 7 88 9 5 6 2 21 4 5 63 6 5 12 3 36
+5
+1 8
+1 4 
+17 20
+8 16 
+2 19 
+88
+8
+36
+63
+88
+
+*/
